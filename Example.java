895285f584c8d84d097b82d410b9738ce2550c67
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-12-29 22:23:18 +0530

    some codes

--- Example.java
+++ Example.java
@@ -3,14 +3,53 @@ class Example{
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        printLine();
 
+        System.out.println("Enter Number One : ");
+        int numberOne = input.nextInt();
 
+        System.out.println("Enter Number two : ");
+        int numberTwo = input.nextInt();
+
+        System.out.println("A. Addition");
+        System.out.println("B. Subtraction");
+        System.out.println("C. Multiplication");
+        System.out.println("D. Division");
+
+        System.out.print("--->");
+        String option = input.next();
+
+        switch (option){
+            case "A":
+                add(numberOne, numberTwo);
+                break;
+            case "B":
+                sub(numberOne, numberTwo);
+                break;
+            case "C":
+            case "D":
+            default:
+                System.out.println("Ended");
+                break;
+        }
+    }
+    //Add method
+    public static void add(int numberOne, int numberTwo){
+        System.out.println("Sum is : " + (numberOne + numberTwo));
 
     }
+    public static void sub(int numberOne, int numberTwo){
 
-    public static void printLine(){
-        System.out.println("============");
-        System.out.println("============");
+
+        if (numberOne > numberTwo){
+            System.out.println("Subtraction is : " + numberOne + " - " + numberTwo + " = " + (numberOne-numberTwo) );
+        }else{
+            System.out.println("Subtraction is : " + numberTwo + " - " + numberOne + " = " + (numberTwo-numberOne) );
+        }
+    }
+    public static void multi(){
+
+    }
+    public static void div(){
+
     }
 }
