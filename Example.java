3d7590dc7c30c33f1e077c5613401a5ec2fabb76
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-12-29 20:31:39 +0530

    some codes

--- Example.java
+++ Example.java
@@ -4,13 +4,25 @@ class Example{
 
         Scanner input = new Scanner(System.in);
 
-        int[][] marks = {
-                {34, 23, 56, 78},
-                {90, 23, 45}
+        int[][][] marks = {
+                {
+                        {23, 34, 45},
+                        {23, 34, 45},
+                        {89, 78, 14}
+                },
+                {
+                        {90, 78, 19},
+                        {65, 23, 56},
+                        {99, 12, 34}
+                },
+                {
+                        {23, 45, 12},
+                        {90, 45, 19},
+                        {99, 44, 22}
+                },
         };
 
-        System.out.println(marks[0].length);
-        System.out.println(marks[1].length);
-
+        System.out.println(marks.length);
+        System.out.println(marks[1][1][2]);
     }
 }
