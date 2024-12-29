4be49e977f1ec7d49c6e9846de4510abac581824
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-12-29 20:53:03 +0530

    some codes

--- Example.java
+++ Example.java
@@ -3,26 +3,14 @@ class Example{
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        printLine();
 
-        int[][][] marks = {
-                {
-                        {23, 34, 45},
-                        {23, 34, 45},
-                        {89, 78, 14}
-                },
-                {
-                        {90, 78, 19},
-                        {65, 23, 56},
-                        {99, 12, 34}
-                },
-                {
-                        {23, 45, 12},
-                        {90, 45, 19},
-                        {99, 44, 22}
-                },
-        };
 
-        System.out.println(marks.length);
-        System.out.println(marks[1][1][2]);
+
+    }
+
+    public static void printLine(){
+        System.out.println("============");
+        System.out.println("============");
     }
 }
