b52416cb938f02346f0524d2a8f9ff0ac6b2f396
Hemru Chamuditha <chamudhitha123@gmail.com>
2025-01-01 19:58:27 +0530

    create classes

--- Example.java
+++ Example.java
@@ -2,54 +2,21 @@ import java.util.Scanner;
 class Example{
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+callMethod();
 
-        System.out.println("Enter Number One : ");
-        int numberOne = input.nextInt();
-
-        System.out.println("Enter Number two : ");
-        int numberTwo = input.nextInt();
-
-        System.out.println("A. Addition");
-        System.out.println("B. Subtraction");
-        System.out.println("C. Multiplication");
-        System.out.println("D. Division");
-
-        System.out.print("--->");
-        String option = input.next();
-
-        switch (option){
-            case "A":
-                add(numberOne, numberTwo);
-                break;
-            case "B":
-                sub(numberOne, numberTwo);
-                break;
-            case "C":
-            case "D":
-            default:
-                System.out.println("Ended");
-                break;
-        }
-    }
-    //Add method
-    public static void add(int numberOne, int numberTwo){
-        System.out.println("Sum is : " + (numberOne + numberTwo));
 
     }
-    public static void sub(int numberOne, int numberTwo){
 
+    public static void callMethod(){
 
-        if (numberOne > numberTwo){
-            System.out.println("Subtraction is : " + numberOne + " - " + numberTwo + " = " + (numberOne-numberTwo) );
-        }else{
-            System.out.println("Subtraction is : " + numberTwo + " - " + numberOne + " = " + (numberTwo-numberOne) );
-        }
+        TestClass testClass = new TestClass();
+
+        System.out.println(testClass.height);
+
+        TestClass testClass1 = new TestClass();
+        testClass1.height = 34.23;
+        System.out.println(testClass1.height);
+        testClass1.methodTwo();
     }
-    public static void multi(){
 
-    }
-    public static void div(){
-
-    }
 }
---
+++ TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+
+    int age = 34;
+    double height = 4.3;
+
+    public void methodOne(){
+        System.out.println("method one");
+    }
+
+    public void methodTwo(){
+        System.out.println("methodTwo");
+    }
+
+}
